34102ac8759e0d6929be19d9675102ce1aa54783
Diego Barral <diego@rootstrap.com>
2019-06-06 16:39:11 -0300

    Login screen

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { func, object } from 'prop-types';
 
+// Assets
+import backgroundImage from 'assets/images/background.png';
+
+// Components
 import LoginForm from 'components/user/LoginForm';
+
+// Actions
 import { login } from 'actions/userActions';
+
+//Others
 import translate from 'utils/i18n';
 import { SIGN_UP_SCREEN } from 'constants/screens';
+
+// Styles
 import styles from './styles';
+import constants from 'styles/common';
 
 const LoginScreen = ({ login, navigation }) => (
   <View style={styles.container}>
-    <Text style={styles.welcome}>
-      {translate('SIGN_IN.title')}
+    <Image source={backgroundImage}
+      style={styles.backgroundImage}>
+    </Image>
+    <Text style={[constants.boldText, styles.title]}>
+      {translate('SIGN_IN.target').toUpperCase()}
     </Text>
     <LoginForm onSubmit={user => login(user.toJS())} />
-    <Button
-      title={translate('SIGN_UP.title')}
-      onPress={() => navigation.push({ component: { name: SIGN_UP_SCREEN } })}
-    />
+    <Text style={[constants.semiBoldText, styles.forgotPassword]}>{translate('SIGN_IN.forgotPassword')}</Text>
+    <Text style={[constants.boldText, styles.facebook]}>{translate('SIGN_IN.facebook').toUpperCase()}</Text>
+    <View style={styles.dividerLine} />
+    <Text style={[constants.semiBoldText, styles.signUp]}>{translate('SIGN_UP.title').toUpperCase()}</Text>
   </View>
 );
 
