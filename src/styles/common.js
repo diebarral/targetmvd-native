db12d3ae2ba8c8be3aceb957ba6dc4f5850190f9
Diego Barral <diego@rootstrap.com>
2019-06-06 12:13:29 -0300

    Add styles that apply to most of the project

---
+++ src/styles/common.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const constants = StyleSheet.create({
+  customFont: {
+    fontFamily: 'OpenSans-Bold'
+  },
+  mainButton: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    height: 37,
+    justifyContent: 'center',
+    width: 114,
+  },
+  mainButtonText: {
+    color: 'white',
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 11,
+    letterSpacing: 1.65,
+    textAlignVertical: 'center'
+  }
+});
+
+export default constants;
